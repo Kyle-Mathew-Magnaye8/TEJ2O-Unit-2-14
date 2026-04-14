88993b8cc0e738e93208bf189c9e0a404a8f2860
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-14 12:48:37 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Mar 2026
+ * This program moves the pixels around the perimeter clockwise and counter clockwise.
 */
 
 basic.showString('Hello, World!')
