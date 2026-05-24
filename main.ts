7d15357b5796176295cf643da98d1a1acd89d3c7
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-05-23 21:33:29 -0400

    change date

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* * Copyright (c) 2020 MTHS All rights reserved 
  * * Created by: Kyle Matthew 
- * Created on: Mar 2026 
+ * Created on: Apr 2026 
  * This program moves the pixels around the perimeter clockwise and counter clockwise. 
  */
 
